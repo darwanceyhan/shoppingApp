3f458f70ac7c2376624cfc2e9a31afa9e365ea29
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-12-28 09:31:44 +0300

    Update: Data filtering based on the name query and category.

--- back-end/query/products.query.ts
+++ back-end/query/products.query.ts
@@ -1,5 +1,24 @@
-export let productsQuery: string = `
-                        SELECT productname, price, stockquantity, categoryname
-                        FROM products
-                        INNER JOIN categories ON products.categoryid = categories.categoryid
-                    `;
+let productsQuery: string = `
+  SELECT 
+    productname,
+    price,
+    stockquantity,
+    categoryname
+  FROM products
+  INNER JOIN categories ON products.categoryid = categories.categoryid
+`;
+
+export const modifiedQuery = (queryName?: string, inQueryFilterNumbers?: string): string => {
+  const filters: string[] = [];
+
+  if (inQueryFilterNumbers) {
+    filters.push(`categories.categoryname IN (${inQueryFilterNumbers})`);
+  }
+
+  if (queryName) {
+    filters.push(`products.productname LIKE '%${queryName}%'`);
+  }
+  const filtersClause = filters.length > 0 ? `WHERE ${filters.length > 1 ? filters.join(' AND ') : filters.join('')}` : '';
+
+  return `${productsQuery}  ${filtersClause}`;
+};
--- back-end/routers/category.router.ts
+++ back-end/routers/category.router.ts
@@ -4,13 +4,17 @@ import { pool } from "../index";
 import { Ierror } from "../interfaces/Ierror";
 import { IcategoryResponse } from "../interfaces/IcategoryResponse";
 import { IproductResponse } from "../interfaces/IproductResponse";
-import { tokenHelper } from "../middlewares/auth.middleware";
 import { isValidCategoryName } from "../helpers/validCategoryName.helper";
-import { productsQuery } from "../query/products.query";
+import { modifiedQuery } from "../query/products.query";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
-    pool.query(productsQuery,
+    const { name } = req.query
+    const queryName = name?.toString().toLowerCase()
+    console.log(name)
+    console.log(modifiedQuery(queryName))
+    const query = modifiedQuery(queryName)
+    pool.query(query,
         (err: Error, result: { rows: IproductResponse }) => {
             if (err) {
                 const errObject: Ierror = {
@@ -43,14 +47,19 @@ router.get(
 
                 const splittedCategoryNames = req.params.categoriesParam.split("&")
                 console.log(splittedCategoryNames)
+                const { name } = req.query
+                console.log(name)
 
                 if (isValidCategoryName(splittedCategoryNames, categories)) {
+                    const queryName = name?.toString().toLowerCase()
+                    const inQueryFilterNumbers = splittedCategoryNames.map((_, index) => `$${index + 1}`).join(',');
+                    console.log(inQueryFilterNumbers)
+                    const query = modifiedQuery(queryName, inQueryFilterNumbers)
+                    console.log(query)
 
-                    const inQueryFilterNumbers = splittedCategoryNames.map((_, index) => `$${index + 1}`).join(", ");
-                    const modifiedQuery = productsQuery + ` WHERE categories.categoryname IN (${inQueryFilterNumbers})`;
-
-                    pool.query(modifiedQuery, splittedCategoryNames, (err: Error, result: { rows: IproductResponse }) => {
+                    pool.query(query, splittedCategoryNames, (err: Error, result: { rows: IproductResponse }) => {
                         if (err) {
+                            console.log(err)
                             const errObject: Ierror = {
                                 message: "Internal Server Error",
                                 statusCode: 500,
@@ -58,7 +67,7 @@ router.get(
                             return next(errObject);
                         } else {
                             console.table(result.rows)
-                            res.json(result.rows);
+                            res.send(result.rows);
                         }
                     });
                 } else {
